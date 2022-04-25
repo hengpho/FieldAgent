1b81c61fc84ee755b8909eb4ac13f94684276678
Heng Pho <hengpho98@gmail.com>
2022-04-24 22:23:36 -0500

    Fin

--- FieldAgent.DAL.Test/TestReportsRepository.cs
+++ FieldAgent.DAL.Test/TestReportsRepository.cs
@@ -11,32 +11,49 @@ namespace FieldAgent.DAL.Test
     {
         ReportsRepository db;
         DBFactory dbf;
-
+        DateTime dateOfBirth = new DateTime(2006, 8, 3);
+        DateTime DeactivationDate = new DateTime(2021, 1, 21);
+        
+        DateTime ActivationDate = new DateTime(2020, 3, 18);
+        DateTime dateOfBirth2 = new DateTime(2020, 11, 12);
+        DateTime DeactivationDate2 = new DateTime(2018, 11, 6);
         [SetUp]
         public void Setup()
         {
-            /*ConfigProvider cp = new ConfigProvider();
+            ConfigProvider cp = new ConfigProvider();
             dbf = new DBFactory(cp.Config, FactoryMode.TEST);
-            db = new ReportsRepository(dbf);
-            dbf.GetDbContext().Database.ExecuteSqlRaw("SetKnownGoodState");*/
+            db = new ReportsRepository(cp.Config);
+            dbf.GetDbContext().Database.ExecuteSqlRaw("SetKnownGoodState");
         }
 
         [Test]
         public void TestGetTopAgents()
         {
+            var result = db.GetTopAgents();
+            Assert.AreEqual("Vinson Leechman", result.Data[0].NameLastFirst);
+            Assert.AreEqual("Edyth Mor", result.Data[1].NameLastFirst);
+            Assert.AreEqual("Kitti Stood", result.Data[2].NameLastFirst);
 
         }
 
         [Test]
-        public void TestGetPensionList()
+        public void TestGetPensionList() // @agencyId - 10, 7
         {
-
+            var result = db.GetPensionList(10);
+            Assert.AreEqual("Maxine", result.Data[0].AgencyName);
+            Assert.AreEqual("Mor Edyth", result.Data[0].NameLastFirst);
+            Assert.AreEqual(dateOfBirth, result.Data[0].DateOfBirth);
+            Assert.AreEqual(DeactivationDate, result.Data[0].DeactivationDate);
         }
 
         [Test]
-        public void TestAuditClearance()
+        public void TestAuditClearance() // sc = 2   // ag id = 7 
         {
-
+            var result = db.AuditClearance(2, 7);
+            Assert.AreEqual("Gribble Kirby", result.Data[0].NameLastFirst);
+            Assert.AreEqual(dateOfBirth2, result.Data[0].DateOfBirth);
+            Assert.AreEqual(ActivationDate, result.Data[0].ActivationDate);
+            Assert.AreEqual(DeactivationDate2, result.Data[0].DeactivationDate);            
         }
     }
 }
--- FieldAgent.DAL/Repository/ReportsRepository.cs
+++ FieldAgent.DAL/Repository/ReportsRepository.cs
@@ -30,30 +30,96 @@ namespace FieldAgent.DAL.Repository
         public Response<List<TopAgentListItem>> GetTopAgents()
         {
             Response<List<TopAgentListItem>> response = new Response<List<TopAgentListItem>>();
+            response.Data = new List<TopAgentListItem>();
             using (var connection = new SqlConnection(connectionString))
             {
-                var cmd = new SqlCommand("GetTopAgents", connection);
+                var cmd = new SqlCommand("TopAgents", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 connection.Open();
 
                 using (var reader = cmd.ExecuteReader())
                 {
-                    // response.Data = reader;
+                    while (reader.Read())
+                    {
+                        response.Data.Add(new TopAgentListItem
+                        {
+                            NameLastFirst = reader["Name"].ToString(),
+                            DateOfBirth = (DateTime)reader["DateOfBirth"],
+                            CompletedMissionCount = (int)reader["NumberOfMissions"]
+                        });
+                        response.Success = true;
+                    }
                 }
             }
 
             return response;
         }
-
         public Response<List<PensionListItem>> GetPensionList(int agencyId)
         {
-            throw new NotImplementedException();
-        }
+            Response<List<PensionListItem>> response = new Response<List<PensionListItem>>();
+            response.Data = new List<PensionListItem>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("PensionList", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
 
+                cmd.Parameters.AddWithValue("@agencyId", agencyId);
+                
+                connection.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        response.Data.Add(new PensionListItem
+                        {
+                            AgencyName = reader["ShortName"].ToString(),
+                            BadgeId = (Guid)reader["BadgeId"],
+                            NameLastFirst = reader["NameLastFirst"].ToString(),
+                            DateOfBirth = (DateTime)reader["DateOfBirth"],
+                            DeactivationDate = (DateTime)reader["DeactivationDate"],
+                        });
+                        response.Success = true;
+                    }
+                }
+            }
+
+            return response;
+        }
+        
         public Response<List<ClearanceAuditListItem>> AuditClearance(int securityClearanceId, int agencyId)
         {
-            throw new NotImplementedException();
+            Response<List<ClearanceAuditListItem>> response = new Response<List<ClearanceAuditListItem>>();
+            response.Data = new List<ClearanceAuditListItem>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("ClearanceAudit", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@securityClearanceId", securityClearanceId);
+                cmd.Parameters.AddWithValue("@agencyId", agencyId);
+
+                connection.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        response.Data.Add(new ClearanceAuditListItem
+                        {
+                            BadgeId = (Guid)reader["BadgeId"],
+                            NameLastFirst = reader["NameLastFirst"].ToString(),
+                            DateOfBirth = (DateTime)reader["DateOfBirth"],
+                            ActivationDate = (DateTime)reader["ActivationDate"],
+                            DeactivationDate = (DateTime)reader["DeactivationDate"],
+                        });
+                        response.Success = true;
+                    }
+                }
+            }
+
+            return response;
         }
     }
 }
